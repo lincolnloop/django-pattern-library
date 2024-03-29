9966188737bea78b5b7d6df526ef5018cca6b6a4
Sanyam Khurana <sanyam.khurana01@gmail.com>
2024-03-30 01:07:27 +0530

    Add syntax highlight for html and md

--- pattern_library/static/pattern_library/src/js/components/syntax-highlighting.js
+++ pattern_library/static/pattern_library/src/js/components/syntax-highlighting.js
@@ -1,9 +1,13 @@
-import hljs from 'highlight.js/lib/core';
-import django from 'highlight.js/lib/languages/django';
-import yaml from 'highlight.js/lib/languages/yaml';
+import hljs from "highlight.js/lib/core";
+import django from "highlight.js/lib/languages/django";
+import yaml from "highlight.js/lib/languages/yaml";
+import html from "highlight.js/lib/languages/vbscript-html.js";
+import md from "highlight.js/lib/languages/markdown.js";
 
-export default function() {
-    hljs.registerLanguage('django', django);
-    hljs.registerLanguage('yaml', yaml);
+export default function () {
+    hljs.registerLanguage("django", django);
+    hljs.registerLanguage("yaml", yaml);
+    hljs.registerLanguage("md", md);
+    hljs.registerLanguage("html", html);
     hljs.highlightAll();
 }
